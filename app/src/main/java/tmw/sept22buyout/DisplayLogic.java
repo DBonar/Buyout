fecf2682c05f959fe16882a0d3a567742ec414d6
DBonar <dbonar>
2018-05-01 19:36:02 -0400

    cherry picked the recent display logic changes into my one-act branch

--- app/src/main/java/tmw/sept22buyout/DisplayLogic.java
+++ app/src/main/java/tmw/sept22buyout/DisplayLogic.java
@@ -33,13 +33,20 @@ public class DisplayLogic extends AppCompatActivity {
     protected java.util.List<LinearLayout> buildLayout(@Nullable View.OnClickListener tokenCallback,
                                                        @Nullable View.OnClickListener chainCallback ) {
 
-        // That vertical stack will be made of a number
+        // We are making a vertical stack made of a number
         // of horizontal rows.  We'll create the rows as
         // more LinearLayouts and partially initialize them.
         // Since they all will have different weights, we'll
         // do the final initializations (setLayoutParams) later
         int nchains = AllChains.instance().getAllChains().length();
-        int totalnrows = Board.BoardYSize + nchains + 6;
+        // 1 for the board
+        // 1 for player's tiles and cash
+        // nchains for the chains
+        // 1 for a space
+        // 1 for the message
+        // 2 more for a spacer
+        // and a final row (details left to the caller)
+        int totalnrows = nchains + 7;
         ArrayList<LinearLayout> hlayout = new ArrayList<LinearLayout>(totalnrows);
         for (int lln = 0; (lln < totalnrows); lln++) {
             LinearLayout temp = new LinearLayout(this);
@@ -47,8 +54,9 @@ public class DisplayLogic extends AppCompatActivity {
             hlayout.add(lln, temp);
             //hlayout[lln].setLayoutParams(hlparams);
         }
+        int rownum = 0;  // will keep track of which row we're inserting
 
-        // layout for the board rows.  They all get weight 1
+        // layout for some board rows.  weight 1
         LinearLayout.LayoutParams hlparams_1 =
                 new LinearLayout.LayoutParams(
                         LinearLayout.LayoutParams.MATCH_PARENT,
@@ -56,38 +64,18 @@ public class DisplayLogic extends AppCompatActivity {
         hlparams_1.width = LinearLayout.LayoutParams.MATCH_PARENT;
         hlparams_1.height = 0;
         hlparams_1.weight = 1;
-        int rownum = 0; // running counter of which row we're working on
-        for (; rownum < Board.BoardYSize; rownum++) {
-            hlayout.get(rownum).setLayoutParams(hlparams_1);
-        }
 
-        // Create the board: a grid of TextView
-        // They need LayoutPrams, so set those up now
-        // then loop over the board creating and adding
-        // the horizontal rows.
+        // A layout for the "buttons".  Used for tiles in the board,
+        // tiles a user might play, and elsewhere.
         LinearLayout.LayoutParams btnparams =
                 new LinearLayout.LayoutParams(
                         LinearLayout.LayoutParams.MATCH_PARENT,
                         LinearLayout.LayoutParams.WRAP_CONTENT);
         btnparams.width = 0;
-        btnparams.height = LinearLayout.LayoutParams.MATCH_PARENT;
+        btnparams.height = LinearLayout.LayoutParams.WRAP_CONTENT;
         btnparams.weight = 1;
 
-        Board board = Board.instance();
-        for (int boardrow = 0; (boardrow < Board.BoardYSize); boardrow++) {
-            for (int boardcol = 0; (boardcol < Board.BoardXSize); boardcol++) {
-                BoardSpace space = board.getSpace(boardcol, boardrow);
-                String spacename = space.getName();
-                TextView element = new TextView(this);
-                element.setText(spacename);
-                element.setLayoutParams(btnparams);
-                hlayout.get(boardrow).addView(element);
-                space.setDisplay(element);
-            }
-        }
-
-        // Now put in a row below the Board with player tokens and cash
-        // This row will have weight 2 in hopes of being big enough to seen
+        // A layout for some other rows with need more space.
         LinearLayout.LayoutParams hlparams_2 =
                 new LinearLayout.LayoutParams(
                         LinearLayout.LayoutParams.MATCH_PARENT,
@@ -95,98 +83,183 @@ public class DisplayLogic extends AppCompatActivity {
         hlparams_2.width = LinearLayout.LayoutParams.MATCH_PARENT;
         hlparams_2.height = 0;
         hlparams_2.weight = 2;
-        hlayout.get(rownum).setLayoutParams(hlparams_2);
+        hlparams_2.bottomMargin = 2;
 
-        TextView lblTokens = new TextView(this);
-        lblTokens.setText("Tokens:");
-        LinearLayout.LayoutParams lblparams =
-                new LinearLayout.LayoutParams(
-                        LinearLayout.LayoutParams.MATCH_PARENT,
-                        LinearLayout.LayoutParams.WRAP_CONTENT);
-        lblparams.width = 0;
-        lblparams.height = LinearLayout.LayoutParams.MATCH_PARENT;
-        lblparams.weight = 1;
-        lblTokens.setLayoutParams(lblparams);
-        hlayout.get(rownum).addView(lblTokens);
+        // Create the board: a grid of TextView in nested LinearLayouts
+        {  // TODO This could be refactored into a Board-related class.
+            // Each hoizontal line needs to be sized.  So, we
+            // can't just use the params from above, we need one
+            // that is sized based on the Text boxes it contains.
+            LinearLayout.LayoutParams row_params =
+                    new LinearLayout.LayoutParams(
+                            LinearLayout.LayoutParams.MATCH_PARENT,
+                            LinearLayout.LayoutParams.MATCH_PARENT);
+            row_params.width = LinearLayout.LayoutParams.MATCH_PARENT;
+            row_params.height = LinearLayout.LayoutParams.WRAP_CONTENT;
+            row_params.weight = 1;
 
-        LinearLayout.LayoutParams widelblparams =
-                new LinearLayout.LayoutParams(
-                        LinearLayout.LayoutParams.MATCH_PARENT,
-                        LinearLayout.LayoutParams.WRAP_CONTENT);
-        widelblparams.width = 0;
-        widelblparams.height = LinearLayout.LayoutParams.MATCH_PARENT;
-        widelblparams.weight = 5;
+            // TODO It would be nice to have the text more centered
+            LinearLayout.LayoutParams cell_params =
+                    new LinearLayout.LayoutParams(
+                            LinearLayout.LayoutParams.MATCH_PARENT,
+                            LinearLayout.LayoutParams.WRAP_CONTENT);
+            cell_params.width = LinearLayout.LayoutParams.MATCH_PARENT;
+            cell_params.height = LinearLayout.LayoutParams.WRAP_CONTENT;
+            cell_params.weight = 1;
+            cell_params.leftMargin = 2;
 
-        BtnScnTokens = new TokenButton[AllTokens.instance().NTokensPerPlayer];
-        for (int tn = 0; (tn < AllTokens.instance().NTokensPerPlayer); tn++) {
-            TokenButton token = new TokenButton(this);
-            token.setText("Button");
-            token.setLayoutParams(btnparams);
-            BtnScnTokens[tn] = token;
-            if (tokenCallback != null) {
-                View vtoke = (View) token;
-                vtoke.setOnClickListener(tokenCallback);
+            // This is the overall element for the board.  Note
+            // that it's height is based on its content just as
+            // the height of the individual rows in it is based on
+            // content.  So the size of this element should be
+            // mostly independant of the screen size.
+            // TODO this is probably ugly on a landscape screen
+            LinearLayout board_layout = new LinearLayout(this);
+            board_layout.setOrientation(LinearLayout.VERTICAL);
+            board_layout.setLayoutParams(row_params);
+
+            // It will have YSize rows each of which is a
+            // horizontal LinearLayout holding XSize 'buttons'
+            Board board = Board.instance();
+            for (int rln = 0; rln < Board.BoardYSize; rln++) {
+                LinearLayout temp = new LinearLayout(this);
+                temp.setOrientation((LinearLayout.HORIZONTAL));
+                temp.setLayoutParams(row_params);
+
+                for (int cln = 0; cln < Board.BoardXSize; cln++) {
+                    BoardSpace space = board.getSpace(cln, rln);
+                    String spacename = space.getName();
+                    TextView element = new TextView(this);
+                    element.setPadding(8,0,0,0);
+                    element.setText(spacename);
+                    element.setLayoutParams(cell_params);
+                    temp.addView(element);
+                    space.setDisplay(element);
+                }
+
+                board_layout.addView(temp);
             }
-            hlayout.get(rownum).addView(token);
+
+            hlayout.set(rownum++, board_layout);  // replacing the orig one.
         }
 
-        LblCash = new TextView(this);
-        LblCash.setText("$" + 30000);
-        LblCash.setLayoutParams(lblparams);
-        hlayout.get(rownum).addView(LblCash);
+
+        // Now put in a row below the Board with player tokens and cash
+        // This row will have weight 2 in hopes of being big enough to seen
+        { // TODO Again, this could probably be factored out in Player-related code
+            LinearLayout player_row = hlayout.get(rownum++);
+
+            player_row.setLayoutParams(hlparams_2);
+
+            TextView lblTokens = new TextView(this);
+            lblTokens.setText("Tokens:");
+            LinearLayout.LayoutParams lblparams =
+                    new LinearLayout.LayoutParams(
+                            LinearLayout.LayoutParams.MATCH_PARENT,
+                            LinearLayout.LayoutParams.WRAP_CONTENT);
+            lblparams.width = 0;
+            lblparams.height = LinearLayout.LayoutParams.MATCH_PARENT;
+            lblparams.weight = 1;
+            lblTokens.setLayoutParams(lblparams);
+            player_row.addView(lblTokens);
+
+            BtnScnTokens = new TokenButton[AllTokens.instance().NTokensPerPlayer];
+            for (int tn = 0; (tn < AllTokens.instance().NTokensPerPlayer); tn++) {
+                TokenButton token = new TokenButton(this);
+                token.setText("Button");
+                token.setLayoutParams(btnparams);
+                BtnScnTokens[tn] = token;
+                if (tokenCallback != null) {
+                    View vtoke = (View) token;
+                    vtoke.setOnClickListener(tokenCallback);
+                }
+                player_row.addView(token);
+            }
+
+            LblCash = new TextView(this);
+            LblCash.setText("$" + 30000);
+            LblCash.setLayoutParams(lblparams);
+            player_row.addView(LblCash);
+        }
 
         // Now we add rows for the chains.
         //  These rows are weight 1 in the stack
-        BtnScnChains = new ChainButton[AllChains.instance().nChains()];
-        LblScnChains = new TextView[AllChains.instance().nChains()];
-        Chain onechain;
-        ListIterator<Chain> chains = new ListIterator<Chain>(AllChains.instance().getAllChains());
-        int chainn = 0;
-        while ((onechain = chains.getNext()) != null) {
-            rownum++;
-            hlayout.get(rownum).setLayoutParams(hlparams_2);  // also need height
+        {  // TODO usual refrain.  Could be factored into Chain-related code
+            BtnScnChains = new ChainButton[AllChains.instance().nChains()];
+            LblScnChains = new TextView[AllChains.instance().nChains()];
+            Chain onechain;
+            ListIterator<Chain> chains = new ListIterator<Chain>(AllChains.instance().getAllChains());
 
-            ChainButton chainbtn = new ChainButton(this, onechain);
-            chainbtn.setText(onechain.toString());
-            chainbtn.setLayoutParams(btnparams);
-            chainbtn.setBackgroundColor(onechain.getChainColor());
-            BtnScnChains[chainn] = chainbtn;
-            if (chainCallback != null) {
-                View vchainbtn = (View) chainbtn;
-                vchainbtn.setOnClickListener(chainCallback);
+            LinearLayout.LayoutParams label_params =
+                    new LinearLayout.LayoutParams(
+                            LinearLayout.LayoutParams.MATCH_PARENT,
+                            LinearLayout.LayoutParams.WRAP_CONTENT);
+            label_params.width = LinearLayout.LayoutParams.WRAP_CONTENT;
+            label_params.height = LinearLayout.LayoutParams.WRAP_CONTENT;
+            label_params.weight = 1;
+            label_params.rightMargin = 5;
+
+            LinearLayout.LayoutParams wide_params =
+                    new LinearLayout.LayoutParams(
+                            LinearLayout.LayoutParams.MATCH_PARENT,
+                            LinearLayout.LayoutParams.WRAP_CONTENT);
+            wide_params.width = 0;
+            wide_params.height = LinearLayout.LayoutParams.MATCH_PARENT;
+            wide_params.weight = 7;
+            wide_params.leftMargin = 10;
+
+            int chainn = 0;
+            while ((onechain = chains.getNext()) != null) {
+                LinearLayout chain_row = hlayout.get(rownum++);
+                chain_row.setLayoutParams(hlparams_2);  // also need height
+
+                ChainButton chainbtn = new ChainButton(this, onechain);
+                chainbtn.setText(onechain.toString());
+                chainbtn.setLayoutParams(label_params);
+                chainbtn.setBackgroundColor(onechain.getChainColor());
+                BtnScnChains[chainn] = chainbtn;
+                if (chainCallback != null) {
+                    View vchainbtn = (View) chainbtn;
+                    vchainbtn.setOnClickListener(chainCallback);
+                }
+                chain_row.addView(chainbtn);
+
+                TextView lblChainStatus = new TextView(this);
+                lblChainStatus.setText("is not on board.");
+                lblChainStatus.setLayoutParams(wide_params);
+                LblScnChains[chainn] = lblChainStatus;
+                chain_row.addView(lblChainStatus);
+                chainn++;
             }
-            hlayout.get(rownum).addView(chainbtn);
-
-            TextView lblChainStatus = new TextView(this);
-            lblChainStatus.setText("is not on board.");
-            lblChainStatus.setLayoutParams(widelblparams);
-            LblScnChains[chainn] = lblChainStatus;
-            hlayout.get(rownum).addView(lblChainStatus);
-            chainn++;
         }
 
         // Finally, add the bottom bits
-        rownum++;
-        hlayout.get(rownum).setLayoutParams(hlparams_1);
-        LblMessage1 = new TextView(this);
-        LblMessage1.setText("Please click the token you wish to place.");
-        hlayout.get(rownum).addView(LblMessage1);
+        { // message location
+            LinearLayout temp = hlayout.get(rownum++);
+            temp.setLayoutParams(hlparams_1);
+            LblMessage1 = new TextView(this);
+            LblMessage1.setText("Please click the token you wish to place.");
+            temp.addView(LblMessage1);
+        }
 
         // Two spacer rows
-        rownum++;
-        hlayout.get(rownum).setLayoutParams(hlparams_1);
-        LblMessage2 = new TextView(this);
-        LblMessage2.setText("");
-        hlayout.get(rownum).addView(LblMessage2);
-        rownum++;
-        hlayout.get(rownum).setLayoutParams(hlparams_1);
-        LblMessage3 = new TextView(this);
-        LblMessage3.setText("");
-        hlayout.get(rownum).addView(LblMessage3);
+        {
+            LinearLayout spacer1 = hlayout.get(rownum++);
+            spacer1.setLayoutParams(hlparams_1);
+            LblMessage2 = new TextView(this);
+            LblMessage2.setText("");
+            spacer1.addView(LblMessage2);
+
+            LinearLayout spacer2 = hlayout.get(rownum++);
+            spacer2.setLayoutParams(hlparams_1);
+            LblMessage3 = new TextView(this);
+            LblMessage3.setText("");
+            spacer2.addView(LblMessage3);
+        }
 
         // set the params for the last row
-        rownum++;
-        hlayout.get(rownum).setLayoutParams(hlparams_2);
+        hlayout.get(rownum++).setLayoutParams(hlparams_2);
+
         return hlayout;
     }
 }
